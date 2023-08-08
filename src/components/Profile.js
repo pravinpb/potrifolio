1df1b0c023ed820b31696e5f654a498293df7cea
Pravin P B <85824492+pravinpb@users.noreply.github.com>
2023-08-08 20:51:00 +0530

    Add files via upload

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,120 @@
+import "./style.css"
+
 const Profile=()=>{
     return(
-        <h1>Profile</h1>
+        <div>
+            <div className="home">
+                <h1 className="home_topic">Profile</h1>
+                <p>
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                    In this competition, more than a thousand people from various states all over South 
+                    India took part, showcasing their amazing performances and winning exciting prizes.
+                    Kishore Kumar participated in the special category and secured the 1st place, 
+                    making him the sole representative in his category for the championship round. 
+                    He excelled in the championship round, scoring the highest marks and winning the 
+                    overall championship in the senior category, which brought pride to the college.
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                    In this competition, more than a thousand people from various states all over South 
+                    India took part, showcasing their amazing performances and winning exciting prizes.
+                    Kishore Kumar participated in the special category and secured the 1st place, 
+                    making him the sole representative in his category for the championship round. 
+                    He excelled in the championship round, scoring the highest marks and winning the 
+                    overall championship in the senior category, which brought pride to the college.
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                    In this competition, more than a thousand people from various states all over South 
+                    India took part, showcasing their amazing performances and winning exciting prizes.
+                    Kishore Kumar participated in the special category and secured the 1st place, 
+                    making him the sole representative in his category for the championship round. 
+                    He excelled in the championship round, scoring the highest marks and winning the 
+                    overall championship in the senior category, which brought pride to the college.
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                    In this competition, more than a thousand people from various states all over South 
+                    India took part, showcasing their amazing performances and winning exciting prizes.
+                    Kishore Kumar participated in the special category and secured the 1st place, 
+                    making him the sole representative in his category for the championship round. 
+                    He excelled in the championship round, scoring the highest marks and winning the 
+                    overall championship in the senior category, which brought pride to the college.
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                    In this competition, more than a thousand people from various states all over South 
+                    India took part, showcasing their amazing performances and winning exciting prizes.
+                    Kishore Kumar participated in the special category and secured the 1st place, 
+                    making him the sole representative in his category for the championship round. 
+                    He excelled in the championship round, scoring the highest marks and winning the 
+                    overall championship in the senior category, which brought pride to the college.
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event.On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                    In this competition, more than a thousand people from various states all over South 
+                    India took part, showcasing their amazing performances and winning exciting prizes.
+                    Kishore Kumar participated in the special category and secured the 1st place, 
+                    making him the sole representative in his category for the championship round. 
+                    He excelled in the championship round, scoring the highest marks and winning the 
+                    overall championship in the senior category, which brought pride to the college.
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                    In this competition, more than a thousand people from various states all over South 
+                    India took part, showcasing their amazing performances and winning exciting prizes.
+                    Kishore Kumar participated in the special category and secured the 1st place, 
+                    making him the sole representative in his category for the championship round. 
+                    He excelled in the championship round, scoring the highest marks and winning the 
+                    overall championship in the senior category, which brought pride to the college.
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                    In this competition, more than a thousand people from various states all over South 
+                    India took part, showcasing their amazing performances and winning exciting prizes.
+                    Kishore Kumar participated in the special category and secured the 1st place, 
+                    making him the sole representative in his category for the championship round. 
+                    He excelled in the championship round, scoring the highest marks and winning the 
+                    overall championship in the senior category, which brought pride to the college.
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                    In this competition, more than a thousand people from various states all over South 
+                    India took part, showcasing their amazing performances and winning exciting prizes.
+                    Kishore Kumar participated in the special category and secured the 1st place, 
+                    making him the sole representative in his category for the championship round. 
+                    He excelled in the championship round, scoring the highest marks and winning the 
+                    overall championship in the senior category, which brought pride to the college.
+                    On the other hand, Gokulanandan Suresh participated in the common category and confidently secured the 6th place, 
+                    adding to the college's achievements in the event. Kishore Kumar G and Gokulanandan, both from the third year Ai&Ds Department, 
+                    participated in the 17th interstate yoga competition held at Periyanaickenpalayam, 
+                    Coimbatore, conducted by Varahi's Mystic Yoga and Sri Maha Sanketha Trust on 30.07.2023.
+                </p>
+            </div>
+
+        </div>
+        
     )
 }
 export default Profile;
